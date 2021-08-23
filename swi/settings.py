55d4fe1fdff36d8db918c853704d013d53b8c3fd
Adriano Regis <adbrum@outlook.com>
2021-08-23 21:58:35 +0100

    Changed config

--- swi/settings.py
+++ swi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^9f#@94zm4hy5em(al0=5*3nobr^pov1cz#a*n9q2svo3zyp#s"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
