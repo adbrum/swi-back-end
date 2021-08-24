2ed51ac23daf179c74ad8335b7efc13389bd3d93
Adriano Regis <adbrum@outlook.com>
2021-08-24 13:27:15 +0100

    Instaled whitnoise

--- swi/settings.py
+++ swi/settings.py
@@ -55,7 +55,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'swi.urls'
 
